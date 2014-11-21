8395289e67a3ac893bc3b271701d405df66b5d27
Masato Hori <rillomas@gmail.com>
2014-11-21 22:03:04 +0900

    Refactored error handling

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::ptr;
 use std::str;
 use std::io::{File,IoResult};
 
-fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+fn compile_shader(src: &str, ty: GLenum) -> Result<GLuint, String> {
     let shader;
     unsafe {
         shader = gl::CreateShader(ty);
@@ -23,9 +23,12 @@ fn compile_shader(src: &str, ty: GLenum) -> GLuint {
             gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
             let mut buf = Vec::from_elem(len as uint - 1, 0u8); // subtract 1 to skip the trailing null character
             gl::GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-            panic!("{}", str::from_utf8(buf.as_slice()).expect("ShaderInfoLog not valid utf8"));
+            match str::from_utf8(buf.as_slice()) {
+                Some(s) => return Err(s.to_string()),
+                None => return Err("Shader compiled failed. Since the ShaderInfoLog is not valid utf8, the error log could not be retrived".to_string())
+            }
         }
-        shader
+        Ok(shader)
     }
 }
 
@@ -36,40 +39,33 @@ fn read_shader(path: &str) -> IoResult<String> {
 }
 
 fn main() {
-    let vss = match read_shader("src\\vertex.glsl") {
-        Ok(s) => s,
-        Err(e) => panic!("Failed reading vertex shader: {}", e),
-    };
-    println!("vs:\n{}",vss);
-    let fss = match read_shader("src\\fragment.glsl") {
-        Ok(s) => s,
-        Err(e) => panic!("Failed reading fragment shader: {}", e),
-    };
-    println!("fs:\n{}",fss);
+    let vss = read_shader("src\\vertex.glsl")
+        .unwrap_or_else(|e| panic!("Failed reading vertex shader: {}", e));
+        
+    // println!("vs:\n{}",vss);
+    let fss = read_shader("src\\fragment.glsl")
+        .unwrap_or_else(|e| panic!("Failed reading fragment shader: {}", e));
+    // println!("fs:\n{}",fss);
 
     let builder = glutin::WindowBuilder::new();
     let r = builder.with_dimensions(300, 300)
         .with_title("rust sample".to_string())
         .build();
-    match r {
-        Ok(window) => {
-            unsafe { window.make_current() };
-            gl::load_with(|symbol| window.get_proc_address(symbol));
+    let window = r.unwrap_or_else(|e| panic!("Error while building window: {}", e));
+    unsafe { window.make_current() };
+    gl::load_with(|symbol| window.get_proc_address(symbol));
 
-            // call gl functions after the symbols are loaded
-            let vs = compile_shader(vss.as_slice(), gl::VERTEX_SHADER);
-            let fs = compile_shader(fss.as_slice(), gl::FRAGMENT_SHADER);
-            unsafe { gl::ClearColor(0.0, 0.0, 1.0, 1.0); }
-
-            while !window.is_closed() {
-                window.wait_events();
-                unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
-                unsafe { gl::Flush(); }
-                window.swap_buffers();
-            }
-        },
-        Err(e) => {
-            println!("Error while building window: {}", e);
-        }
+    // call gl functions after the symbols are loaded
+    let vs = compile_shader(vss.as_slice(), gl::VERTEX_SHADER)
+        .unwrap_or_else(|e| panic!("Failed compiling vertex shader: {}", e));
+    let fs = compile_shader(fss.as_slice(), gl::FRAGMENT_SHADER)
+        .unwrap_or_else(|e| panic!("Failed compiling fragment shader: {}", e));
+        
+    unsafe { gl::ClearColor(0.0, 0.0, 1.0, 1.0); }
+    while !window.is_closed() {
+        window.wait_events();
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+        unsafe { gl::Flush(); }
+        window.swap_buffers();
     }
 }
