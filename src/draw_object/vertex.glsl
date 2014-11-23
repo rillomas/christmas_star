813bb09e0cf9154b3b960677c4d6e9fe459b2b78
Masato Hori <rillomas@gmail.com>
2014-11-23 18:42:27 +0900

    fixed bug where position data were not all used

--- src/draw_object/vertex.glsl
+++ src/draw_object/vertex.glsl
@@ -1,6 +1,6 @@
 #version 430
 
-layout (location = 0) in vec2 position;
+layout (location = 0) in vec4 position;
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = position;
 }
